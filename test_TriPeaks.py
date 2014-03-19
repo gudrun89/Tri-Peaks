6e3d8a81f4d4c29ee5e46b8625449ad9c874c693
kar8 <kar8@hi.is>
2014-03-19 17:32:45 +0000

    Create test_TriPeaks.py

---
+++ test_TriPeaks.py
@@ -0,0 +1,67 @@
+import unittest
+from Card import *
+from Deck import *
+from TriPeaks import*
+
+class Test(unittest.TestCase):
+	def test_Card1(self):
+		#make sure that cards are different
+		t1 = Card('H',10)
+		t2 = Card('H',11)
+		self.assertNotEqual(t1,t2)
+		
+	def test_Card2(self):
+		#Make sure that __eq__ that checks if cards are teh same, works
+		t1 = Card('H',10)
+		t2 = Card('H',11)
+		t4 = t1.__eq__(t2)
+		self.assertEqual(t4,False)
+		
+	def test_Card3(self):
+		#same
+		t1 = Card('H',11)
+		t2 = Card('H',11)
+		t4 = t1.__eq__(t2)
+		self.assertEqual(t4,True)
+		
+
+	def test_shuffleCard(self):
+		# make sure that shuffle does not loose cards
+		self.seq = range(10)
+		random.shuffle(self.seq)
+		self.seq.sort()
+		self.assertEqual(self.seq, range(10))
+	
+	def test_TriPeaks_decksize(self):
+	#make sure that decksize is the same
+		ta = TriPeaks()
+		tb = TriPeaks()
+		t1 = ta.deckSize()
+		t2 = tb.deckSize()
+		self.assertEqual(t1,t2)
+		
+	def test_TriPeaks2(self):
+		# checks if initboard makes same size board each time
+		t1 = TriPeaks()
+		t2 = TriPeaks()
+		tx = t1.initBoard()
+		ty = t2.initBoard()
+		self.assertEqual(tx, ty)
+		
+	
+	def test_TriPeaks_Highscore(self):
+		#checks if highscore works
+		t1 = TriPeaks()
+		x = t1.addScore(500)
+		self.assertGreater(t1.score, 250)
+		
+	def test_TriPeaks_time(self):
+		#checks if the time keeping works
+		t1 = TriPeaks()
+		time = t1.elapsedTime
+		self.assertGreater(time, 0)
+		
+
+		
+if __name__== '__main__':
+  unittest.main()
