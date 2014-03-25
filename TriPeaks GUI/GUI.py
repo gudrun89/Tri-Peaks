d7b1a0fb86d28855882eceef85d70c902bab585f
gudrun89 <gudrun89@gmail.com>
2014-03-25 12:45:20 +0000

    Hreinsaði GUI kóðann og skjalaði

--- TriPeaks GUI/GUI.py
+++ TriPeaks GUI/GUI.py
@@ -4,78 +4,94 @@ from TriPeaks import *
 
 class TriPeaksGUI(object):
 
-    FPS = 30 # frames per second, the general speed of the program
-    WINDOWWIDTH = 1000 # size of window's width in pixels
-    WINDOWHEIGHT = 600 # size of windows' height in pixels
-    CARDWIDTH = 73 # size of card width in pixels
-    CARDHEIGHT = 96 # size of card height in pixels
-    OFFSETX = CARDWIDTH/2.0    # to find the midpoint of card
-    OFFSETY = CARDHEIGHT/2.0   # to find the midpoint of card
-    startx = 100   # start drawing cards at this point
-    starty = 100   # start drawing cards at this point
-    GAPSIZE = 10 # size of gap between cards in pixels
-    BOARDCOLS = 10 # number of columns of cards
-    BOARDROWS = 4 # number of rows of cards
-    BGCOLOR = (255,255,255) # white background color
+    FPS = 30                    # Frames per second, the general speed of the program
+    WINDOWWIDTH = 1000          # Window's width in pixels
+    WINDOWHEIGHT = 600          # Windows' height in pixels
+    CARDWIDTH = 73              # Card width in pixels
+    CARDHEIGHT = 96             # Card height in pixels
+    OFFSETX = CARDWIDTH/2.0     # To find the midpoint of card
+    OFFSETY = CARDHEIGHT/2.0    # To find the midpoint of card
+    startx = 100                # Start drawing the board cards at this point
+    starty = 100                # Start drawing the board cards at this point
+    GAPSIZE = 10                # Size of gap between cards in pixels
+    BOARDCOLS = 10              # Number of columns of cards
+    BOARDROWS = 4               # Number of rows of cards
+    BGCOLOR = (255,255,255)     # White background color
 
     def __init__(self):
 
         self.game = TriPeaks()
-        self.selectedCard = None
+        self.selectedCard = None    # The card selected with the mouse
 
+        self.mousex = 0 # x coordinate of mouse event
+        self.mousey = 0 # y coordinate of mouse event
+        
+        self.heapRect = pygame.Rect(400, 450, self.CARDWIDTH, self.CARDHEIGHT)  # Rectangle around the heap cards
+        self.deckRect = pygame.Rect(100, 450, self.CARDWIDTH, self.CARDHEIGHT)  # Rectangle around the deck cards
+
+    # Pre:  A TriPeaksGUI object has been created
+    # Post: The main game function is running until the game is stopped
+    # Run:  TriPeaksGUI.gameLoop()
     def gameLoop(self):
         
         global FPSCLOCK, DISPLAYSURF
         pygame.init()
-        FPSCLOCK = pygame.time.Clock()
-        DISPLAYSURF = pygame.display.set_mode((self.WINDOWWIDTH, self.WINDOWHEIGHT))
+        FPSCLOCK = pygame.time.Clock()  # Clock that updates the screen with the frame rate FPS
+        DISPLAYSURF = pygame.display.set_mode((self.WINDOWWIDTH, self.WINDOWHEIGHT))        # The game display surface
 
-        self.mousex = 0 # x coordinate of mouse event
-        self.mousey = 0 # y coordinate of mouse event
-        self.heapRect = pygame.Rect(400, 450, self.CARDWIDTH, self.CARDHEIGHT)
-        self.deckRect = pygame.Rect(100, 450, self.CARDWIDTH, self.CARDHEIGHT)
-        
-        pygame.display.set_caption('Tri Peaks')
+        pygame.display.set_caption('Tri Peaks')     # Window title
         self.initCardsPos()
 
-        DISPLAYSURF.fill(self.BGCOLOR)  # sets the background color
+        DISPLAYSURF.fill(self.BGCOLOR)  # Sets the background color
 
-        while True: # main game loop
+        # Main game loop
+        while True: 
             
-            DISPLAYSURF.fill(self.BGCOLOR) # drawing the window
-            self.drawBoard()
-            
-            for event in pygame.event.get(): # event handling loop
+            DISPLAYSURF.fill(self.BGCOLOR)      # Draws the window
+            self.drawBoard()                    # Draws the game board
+
+            # Event handling loop
+            for event in pygame.event.get(): 
                 if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                     pygame.quit()
                     sys.exit()
+                # Event when the mouse is moved
                 elif event.type == MOUSEMOTION:
                     self.onMouseMove(event)
+                # Event when the mouse button is down
                 elif event.type == MOUSEBUTTONDOWN:
                     self.onMouseDown(event)
+                # Event when the mouse button has been released
                 elif event.type == MOUSEBUTTONUP:
                     self.onMouseReleased(event)
 
                 
-            
-        # Redraw the screen and wait a clock tick.
+            # Redraws the screen and waits a clock tick of one frame rate
             pygame.display.update()
             FPSCLOCK.tick(self.FPS)
 
+    # Pre:  event is a pygame.event object
+    # Post: If a movable card has been selected with the mouse, it follows the mouse motion
+    # Run:  TriPeaksGUI.onMouseMove(event)
     def onMouseMove(self, event):
         if self.selectedCard is not None and self.game.isMovable(self.selectedCard.row, self.selectedCard.col):
             self.selectedCard.moveTo(event.pos[0]-self.OFFSETX, event.pos[1]-self.OFFSETY)
 
-
+    # Pre:  event is a pygame.event object
+    # Post: If a movable card has been selected with the mouse, it becomes the selected card. If the card deck
+    #       is clicked, then a card is moved to the heap
+    # Run:  TriPeaksGUI.onMouseDown(event)
     def onMouseDown(self, event):
         card = self.posToCard(*event.pos)
         if card is not None and self.game.isMovable(card.row, card.col):
             self.selectedCard = self.posToCard(*event.pos)
-            print self.selectedCard
         elif self.deckRect.collidepoint(*event.pos):
             self.game.toHeap()
 
-
+    # Pre:  event is a pygame.event object
+    # Post: If a legal card was selected then it moves to the heap if it collides with the heap card,
+    #       otherwise the card is moved back to its position in the board
+    # Run:  TriPeaksGUI.onMouseReleased(event)
     def onMouseReleased(self, event):
         if self.selectedCard is not None:
             x, y = self.cardToPos(self.selectedCard.row, self.selectedCard.col)
@@ -87,7 +103,9 @@ class TriPeaksGUI(object):
                 self.selectedCard.moveTo(x,y)
             self.selectedCard = None
 
-
+    # Pre:  A TriPeaksGUI object has been created
+    # Post: The cards in the board are assigned their positions 
+    # Run:  TriPeaksGUI.initCardsPos()
     def initCardsPos(self):
         # set initial card positions
         for row in range(self.BOARDROWS):
@@ -96,19 +114,21 @@ class TriPeaksGUI(object):
                     self.game.board[row][col].cardx = self.startx + col*(self.CARDWIDTH+self.GAPSIZE) + (3-row)*self.GAPSIZE*4
                     self.game.board[row][col].cardy = self.starty + row*(0.6*self.CARDHEIGHT)
 
-
+    # Pre:  A TriPeaksGUI object has been created
+    # Post: The game board has been drawn
+    # Run:  TriPeaksGUI.drawBoard()
     def drawBoard(self):
 
-        # show deck
+        # Shows deck
         if self.game.deckSize() > 0:
             DISPLAYSURF.blit(pygame.image.load('panda.png'), (100, 450))
 
-        # show heap
+        # Shows heap
         for (i, card) in enumerate(self.game.heap):
             cardImg = pygame.image.load(card.img)
             DISPLAYSURF.blit(cardImg, (400, 450))
         
-        # show cards in board
+        # Shows cards in board
         for row in range(self.BOARDROWS):
             for col in range(self.BOARDCOLS):
                 if self.game.board[row][col] is not None:
@@ -116,24 +136,25 @@ class TriPeaksGUI(object):
                         cardImg = pygame.image.load(self.game.board[row][col].img).convert()
                     else:
                         cardImg = pygame.image.load('panda.png').convert()
-                    #print '(', game.board[row][col].cardx, ', ', game.board[row][col].cardy
                     DISPLAYSURF.blit(cardImg, (self.game.board[row][col].cardx, self.game.board[row][col].cardy))
 
 
+    # Pre:  row and col are integers
+    # Post: Returns the left, top coordinates of the card at row, col
+    # Run:  TriPeaksGUI.cardToPos(row, col)
     def cardToPos(self, row, col):
-        # returns left, top coordinates of card
-        #(startx + col*(CARDWIDTH+GAPSIZE) + (3-row)*GAPSIZE*4, starty + row*(0.6*CARDHEIGHT))
         left = self.startx + col * (self.CARDWIDTH + self.GAPSIZE) + (3-row)*self.GAPSIZE*4
         top = self.starty + row * 0.6*(self.CARDHEIGHT)
         return (left, top)
 
 
+    # Pre:  x and y are integers
+    # Post: Returns the card at window coordinates (x,y), returns None if there is no card there
+    # Run:  TriPeaksGUI.posToCard(x,y)
     def posToCard(self, x,y):
-        # returns indices of card at coordinates (x,y)
         for cardCol in range(self.BOARDCOLS):
             for cardRow in range(self.BOARDROWS):
                 left, top = self.cardToPos(cardRow, cardCol)
-                #print game.board[cardRow][cardCol], left, top
                 cardRect = pygame.Rect(left, top, self.CARDWIDTH, self.CARDHEIGHT)
                 if cardRect.collidepoint(x, y) and self.game.isMovable(cardRow, cardCol):
                     return self.game.board[cardRow][cardCol]
@@ -141,5 +162,5 @@ class TriPeaksGUI(object):
 
 
 if __name__ == '__main__':
-    TriPeaks = TriPeaksGUI()
-    TriPeaks.gameLoop()
+    TriPeaks = TriPeaksGUI()        # Creates a new TriPeaksGUI object
+    TriPeaks.gameLoop()             # Runs the game loop
